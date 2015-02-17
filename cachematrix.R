41513ad471c4efdf19526187bab28ef717464484
Stephane <stephane.maraut@gmail.com>
2015-02-17 11:15:56 +0100

    R functions with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,16 @@
-## The global idea is to create two functions to cache a matrix and its inverse matrix, and to build a set 
-## of functions (set, get and solve) to manipulate these two matrix.
+## The global idea is to compute the inverse of a matrix and to cache it to avoid redondant calculation as long
+## as the matrix does not change.
+## For this purpose we use the R scoping rules to preserve the state of objects (here a Matrix and its inverse)
+## into another object (here a list of functions), what we will call cached.
 
 
-## makeCacheMatrix constructs 4 functions and returns them in a list
+####################################
+####      makeCacheMatrix       ####
+####################################
+## This function constructs 4 functions (getMat, setMat, getInv and setInv) and returns them in a list
 ## getMat and getInv return respectively the value of the matrix mat and the inverse matrix inv
 ## setMat sets the value of the matrix mat to a new value and initializes the inverse matrix inv to NULL
 ## setInv sets the value of the inverse matrix inv
-
 ## When makeCacheMatrix is invoked, it creates these 4 functions in a specific environment (child of the global environment)
 ## where the variables mat (matrix) and inv (inverse matrix) are stored (or cached) (lexical scoping rule)
 
@@ -33,8 +37,11 @@ makeCacheMatrix <- function(mat = matrix()) {
 }
 
 
-## cacheSolve calculates (if necessary) and returns the inverse of a matrix
-## This function looks for the inverse matrix in the specific environment where the elements of x were created
+####################################
+####        cacheSolve          ####
+####################################
+## This function calculates (if necessary) and returns the inverse of a matrix
+## It looks for the inverse matrix in the specific environment where the elements of x were created
 ## If calculated, the inverse matrix is returned
 ## If not calculated, calculates the inverse matrix, caches it and returns it
 ## The input x is the list of 4 functions returned by the makeCacheMatrix function
